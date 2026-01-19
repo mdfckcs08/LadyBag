5ed3442158b4fb3441d37a38e6a50cc054aa2353
mdfckcs08 <mdfckcs08@gmail.com>
2026-01-18 18:04:07 -0600

    supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,6 @@
 // REEMPLAZA ESTOS VALORES CON LOS TUYOS DE SUPABASE
 const SUPABASE_URL = 'https://qnkqtqdwyzevpqkxcefr.supabase.co';
-const SUPABASE_KEY = 'ElCsActivo0121';
+const SUPABASE_KEY = 'sb_publishable_Fppq0Cd6ghnNy1jzDSgPUQ__YH3YROc';
 
 // Inicializar Supabase
 const { createClient } = supabase;
